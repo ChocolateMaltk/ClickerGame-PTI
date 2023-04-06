610ccbb7eb75b9c70637062813f437134eda22a2
malik <malik.bazil@student.umn.ac.id>
2023-04-06 22:39:06 +0700

    Autoclick sudah berfungsi Setiap kota punya nilai-nilai autoclick sendirinya rek... Ngakak cik!

--- clickerscript.js
+++ clickerscript.js
@@ -3,7 +3,7 @@
 let cash = BigInt(999999999999999999);
 cash = BigInt(0);
 let multiplier = 1;
-let passive_gain = 0;
+let passive_gain = BigInt(0);
 let clickVal = 1;
 
 // Cash-related scripts
@@ -46,6 +46,11 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
   const multiplierCount = document.getElementById("multiplierCount");
   updateMultiplierDisplay();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const passiveGainDisplay = document.getElementById("passiveGain");
+  updatepassiveGainDisplay();
 });
 
 // Script untuk Button di tengah.
@@ -98,22 +103,22 @@ function checkCash(requiredCash, button) {
 
 // Check cv1 button every 500 milliseconds
 setInterval(() => {
-  checkCash(10, up1);
+  checkCash(100, up1);
 }, 500);
 
 // Check cv2 button every 500 milliseconds
 setInterval(() => {
-  checkCash(100, up2);
+  checkCash(1000, up2);
 }, 500);
 
 // Check cv3 button every 500 milliseconds
 setInterval(() => {
-  checkCash(1000, up3);
+  checkCash(10000, up3);
 }, 500);
 
 // Check cv4 button every 500 milliseconds
 setInterval(() => {
-  checkCash(5000, up4);
+  checkCash(100000, up4);
 }, 500);
 
 // Script untuk upgrade multiplier
@@ -147,10 +152,75 @@ function billion() {
   window.alert("Doa Terkabul");
 }
 
-//auto klik
-function autoclik() {
-  for (let i = 0; i < 100; i++) {
-    cash = cash + BigInt(clickVal) * BigInt(multiplier);
+function auto_click(button_id) {
+  let button = document.getElementById(button_id);
+  let required_cash = parseInt(button.getAttribute("data-required-cash"));
+  let gain = parseInt(button.getAttribute("gain"));
+
+  if (cash >= required_cash) {
+    cash -= BigInt(required_cash);
+    passive_gain += BigInt(gain);
+    button.setAttribute("disabled", true);
+    updateCashDisplay();
+    updatepassiveGainDisplay();
+  } else {
+    alert("Uang Anda tidak cukup! Anda perlu ${requiredCash} untuk\nmembuka cabang di sini!");
   }
-  updateCashDisplay();
 }
+
+// Check Autoclick
+
+function check_ac_status(city_id) {
+  const requiredCash = parseInt(document.getElementById(city_id).getAttribute("data-required-cash"));
+  const button = document.getElementById(city_id);
+  let clicked = false;
+  if (cash >= BigInt(requiredCash) && clicked == false) {
+    button.disabled = false;
+    button.style.opacity = "1";
+    button.style.cursor = "pointer";
+    clicked = true;
+    updatepassiveGainDisplay();
+  } else {
+    button.disabled = true;
+    button.style.opacity = "0.5";
+    button.style.cursor = "not-allowed";
+  }
+}
+
+setInterval(function () {
+  check_ac_status("ac-1");
+}, 100);
+setInterval(function () {
+  check_ac_status("ac-2");
+}, 100);
+setInterval(function () {
+  check_ac_status("ac-3");
+}, 100);
+setInterval(function () {
+  check_ac_status("ac-4");
+}, 100);
+setInterval(function () {
+  check_ac_status("ac-5");
+}, 100);
+setInterval(function () {
+  check_ac_status("ac-6");
+}, 100);
+setInterval(function () {
+  check_ac_status("ac-7");
+}, 100);
+setInterval(function () {
+  check_ac_status("ac-8");
+}, 100);
+
+function doAutoClick() {
+  cash += BigInt(passive_gain);
+}
+
+setInterval(function () {
+  doAutoClick();
+}, 1000);
+
+setInterval(function () {
+  updateCashDisplay();
+  updatepassiveGainDisplay();
+}, 100);
